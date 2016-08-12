e1c6e3ab3d740521e29bb130c662416e29a8e6dc
kafka90 <jongjin0308@naver.com>
2016-08-12 08:04:13 +0000

    first

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+Checklist.create(title: '우리 딸램')
+Additive.create(name: '니코틴산아미드', english: 'Nicotinesanmide', information: '니코틴산마이드는 주로 서부 유럽에서 발견되며 습한 환경에서 알레르기를 유발하는 주요한 물질입니다')
